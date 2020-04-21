16535641683d286773acbf9f14ac1ae22bedf482
Myjinji-my <7350513+myjinjimy@user.noreply.gitee.com>
2020-04-21 10:51:53 +0800

    完成自定义验证规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="login-container">
     <!-- el-form表单组件 ,必须使用el-form-item组件包裹-->
-  <el-form :model="user" ref="form" class="login-form" :rules="formRules">
+  <el-form :model="user" ref="login-form" class="login-form" :rules="formRules">
        <div class="login-head">
          <div class="logo"></div>
        </div>
@@ -10,6 +10,9 @@
     1.必须给el-from 组件绑定model为表单数据对象
     2.给需要验证的表单项绑定 prop属性
     3.通过 el-from组件的rules属性配置验证规则
+
+    手动触发表单验证 1.给el-form设置ref 起个名字不重复
+    2.通过ref获取el-form组件 调用组件的validate验证
    -->
   <el-form-item prop="mobile">
     <el-input
@@ -23,8 +26,8 @@
       placeholder="输入验证码"
     ></el-input>
   </el-form-item>
-  <el-form-item>
-    <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  <el-form-item prop="agree">
+    <el-checkbox v-model="user.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
   </el-form-item>
   <el-form-item>
     <el-button class="login-btn" type="primary" @click="onLogin" :loading="loginLoading">登录</el-button>
@@ -44,9 +47,10 @@ export default {
     return {
       user: {
         moblic: '', // 手机号
-        code: ''// 验证码
+        code: '', // 验证码
+        agree: false // 是否同意协议
       },
-      checked: true,
+      // checked: true,
       loginLoading: false,
       formRules: { // 表单验证规则匹配
         // 要验证的数据名称: 规则列表[]
@@ -57,6 +61,21 @@ export default {
         code: [
           { required: true, message: '验证码不能为空', trigger: 'change' },
           { pattern: /^\d{6}$/, message: '请输入正确的验证码', trigger: 'change' }
+        ],
+        agree: [
+          { // 自定义效验规则
+            // 验证通过 callback()
+            // 验证失败 callback(new Error('错误信息'))
+            validator: (rule, value, callback) => {
+              if (value) {
+                callback()
+              } else {
+                callback(new Error('请同意用户协议'))
+              }
+            },
+            // message: '勾选用户协议',
+            trigger: 'change'
+          }
         ]
       }
     }
@@ -69,14 +88,22 @@ export default {
     onLogin () {
     // 获取表单数据
     // 表单验证,验证通过,处理后端响应
-      const user = this.user
-
+      this.$refs['login-form'].validate(valid => {
+        if (!valid) { // 验证失败 停止请求提交
+          return
+        }
+        // 验证通过 请求登录
+        this.login()
+      })
+    },
+    login () {
+      // const user = this.user
       // 开启登录中的 loading
       this.loginLoading = true
       request({
         method: 'POST',
         url: '/mp/v1_0/authorizations',
-        data: user
+        data: this.user
       }).then(res => {
         console.log(res)
         this.$message({
